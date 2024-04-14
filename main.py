b91d9f9a83e9ad6faf243f527eca965482e53247
winning godspower <winninggodspower@gmail.com>
2024-04-14 09:08:14 +0100

    feat: add response webhook

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, BackgroundTasks
 from pydantic import BaseModel, EmailStr
 
+import requests
 from scrape import run_scrape
 
 app = FastAPI()
-
+resonse_webhook_url = "https://hook.integrator.boost.space/k80rinp9fgzwhlysiohlvy12x8r0qa36"
 
 class UserCredential(BaseModel):
     email: EmailStr
@@ -24,13 +25,22 @@ async def root():
     
 
 @app.post("/")
-async def main(user_credential: UserCredential):
+async def main(user_credential: UserCredential, background_tasks: BackgroundTasks):
+    background_tasks.add_task(
+        run_scrape_and_send_webhook, 
+        user_credential.email,
+        user_credential.password,
+    )
+    return {'message': f'Scraping in progress. Check webhook for results. 👉 {resonse_webhook_url}'}
+    
+
+async def run_scrape_and_send_webhook(email: EmailStr, password: str):
     try:
-        copied_text = run_scrape(
-                        user_credential.email,
-                        user_credential.password,
-                    )
-        return {"copied_text": copied_text}
+        copied_text = run_scrape(email, password)
+        send_webhook({"copied_text": copied_text})
     except Exception as error:
         print("An error occurred:", type(error).__name__, "–", error)
-        raise HTTPException(status_code=400, detail='something went wrong')
+    
+def send_webhook(response):
+    print('sending weebhook response')
+    requests.post(resonse_webhook_url, url=response)
